79fdb8a6c6695db50c4c6234123aaece9f3fa733
Pascal Wittler <pasi.w@web.de>
2020-02-08 20:19:36 +0100

    bring clock to life

--- templates/res/js/main.js
+++ templates/res/js/main.js
@@ -1,19 +1,45 @@
 class Clock {
-  constructor() {
-    this.hour = 0;
-    this.minute = 0;
-    this.second = 0;
+  constructor(hour, minute, second, hourHand, minuteHand, secondHand) {
+    this.hourHand = hourHand;
+    this.minuteHand = minuteHand;
+    this.secondHand = secondHand;
+    this.hourHandPosition = hour * 30 + minute / 2;
+    this.minuteHandPosition = minute * 6 + second / 10;
+    this.secondHandPosition = second * 6;
   }
 
-  setHourHand(hour) {
-    this.hour = hour;
+  setCurrentTime() {
+    const now = new Date();
+    this.hourHandPosition = now.getHours() * 30 + now.getMinutes() / 2;
+    this.minuteHandPosition = now.getMinutes() * 6 + now.getSeconds() / 10;
+    this.secondHandPosition = now.getSeconds() * 6;
   }
 
-  setMinuteHand(minute) {
-    this.minute = minute;
-  }
-
-  setSecondHand(second) {
-    this.second = second;
+  rotateClockHands() {
+    this.hourHand.setAttribute(
+      'style',
+      'transform: translate(-50%, -50%) rotate(' + this.hourHandPosition + 'deg);'
+    );
+    this.minuteHand.setAttribute(
+      'style',
+      'transform: translate(-50%, -50%) rotate(' + this.minuteHandPosition + 'deg);'
+    );
+    this.secondHand.setAttribute(
+      'style',
+      'transform: translate(-50%, -50%) rotate(' + this.secondHandPosition + 'deg);'
+    );
   }
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+  let clock = new Clock(0, 0, 0,
+    document.querySelector('.hour-hand'),
+    document.querySelector('.minute-hand'),
+    document.querySelector('.second-hand')
+  );
+
+  window.setInterval(function() {
+    clock.setCurrentTime();
+    clock.rotateClockHands();
+  }, 1000);
+});
